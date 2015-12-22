980be0ccde388f656460859fd1af2de559717c91
nkarpey <dev.karpey@outlook.com>
2015-12-22 19:32:38 +0500

    MDL Loader/Info

---
+++ Core/MDL/MdlSpecification.cs
@@ -0,0 +1,395 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class MdlSpecification
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct studiohdr_t
+    {
+        public int id; // Model format ID, such as "IDST" (0x49 0x44 0x53 0x54)
+        public int version; // Format version number, such as 48 (0x30,0x00,0x00,0x00)
+
+        public int checksum;
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 64)]
+        public char[] name; // The internal name of the model, padding with null bytes.
+
+        public int dataLength; // Data size of MDL file in bytes.
+
+        public Vector3 eyeposition; // Position of player viewpoint relative to model origin
+        public Vector3 illumposition; // ?? Presumably the point used for lighting when per-vertex lighting is not enabled.
+        public Vector3 hull_min; // Corner of model hull box with the least X/Y/Z values
+        public Vector3 hull_max; // Opposite corner of model hull box
+        public Vector3 view_bbmin;
+        public Vector3 view_bbmax;
+        
+        public int flags; // Binary flags in little-endian order. 
+        
+        // mstudiobone_t
+        public int bone_count;    // Number of data sections (of type mstudiobone_t)
+        public int bone_offset; // Offset of first data section
+        
+        // mstudiobonecontroller_t
+        public int bonecontroller_count;
+        public int bonecontroller_offset;
+        
+        // mstudiohitboxset_t
+        public int hitbox_count;
+        public int hitbox_offset;
+        
+        // mstudioanimdesc_t
+        public int localanim_count;
+        public int localanim_offset;
+        
+        // mstudioseqdesc_t
+        public int localseq_count;
+        public int localseq_offset;
+        
+        public int activitylistversion;
+        public int eventsindexed;
+
+        // mstudiotexture_t
+        public int texture_count;
+        public int texture_offset;
+
+        public int texturedir_count;
+        public int texturedir_offset;
+
+        public int skinreference_count;
+        public int skinrfamily_count;
+        public int skinreference_index;
+        
+        // mstudiobodyparts_t
+        public int bodypart_count;
+        public int bodypart_offset;
+            
+        // mstudioattachment_t
+        public int attachment_count;
+        public int attachment_offset;
+
+        public int localnode_count;
+        public int localnode_index;
+        public int localnode_name_index;
+        
+        // mstudioflexdesc_t
+        public int flexdesc_count;
+        public int flexdesc_index;
+        
+        // mstudioflexcontroller_t
+        public int flexcontroller_count;
+        public int flexcontroller_index;
+        
+        // mstudioflexrule_t
+        public int flexrules_count;
+        public int flexrules_index;
+
+        // mstudioikchain_t
+        public int ikchain_count;
+        public int ikchain_index;
+
+        // mstudiomouth_t
+        public int mouths_count; 
+        public int mouths_index;
+        
+        // mstudioposeparamdesc_t
+        public int localposeparam_count;
+        public int localposeparam_index;
+
+        public int surfaceprop_index;
+
+        public int keyvalue_index;
+        public int keyvalue_count;    
+
+        // mstudioiklock_t
+        public int iklock_count;
+        public int iklock_index;
+
+        public float mass;
+        public int contents;    
+
+        // mstudiomodelgroup_t
+        public int includemodel_count;
+        public int includemodel_index;
+        
+        public int virtualModel; // Placeholder for mutable-void*
+        
+        // mstudioanimblock_t
+        public int animblocks_name_index;
+        public int animblocks_count;
+        public int animblocks_index;
+        
+        public int animblockModel; // Placeholder for mutable-void*
+
+        public int bonetablename_index;
+        
+        public int vertex_base; 
+        public int offset_base;
+        
+        // Used with $constantdirectionallight from the QC 
+        // Model should have flag #13 set if enabled
+        public byte directionaldotproduct;
+        
+        public byte rootLod; // Preferred rather than clamped
+        
+        // 0 means any allowed, N means Lod 0 -> (N-1)
+        public byte numAllowedRootLods;    
+        
+        public byte unused;
+        public int unused2; 
+        
+        // mstudioflexcontrollerui_t
+        public int flexcontrollerui_count;
+        public int flexcontrollerui_index;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudiobone_t
+    {
+        public int sznameindex;
+        public int parent; // parent bone
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 6)]
+        public int[] bonecontroller; // bone controller index, -1 == none
+
+        public Vector3 pos;
+        public Quaternion quat;
+        public Vector3 rot;
+
+        public Vector3 posscale;
+        public Vector3 rotscale;
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 12)]
+        public float[] poseToBone;
+
+        public Quaternion qAlignment;
+        public int flags;
+        public int proctype;
+        public int procindex; // procedural rule
+        public int physicsbone; // index into physically simulated bone
+        public int surfacepropidx; // index into string tablefor property name
+        public int contents; // See BSPFlags.h for the contents flags
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8)]
+        public int[] unused;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudiotexture_t
+    {
+        public int sznameindex;
+        public int flags;
+        public int used;
+        public int unused1;
+        public int material; // fixme: this needs to go away . .isn't used by the engine, but is used by studiomdl
+        public int clientmaterial; // gary, replace with client material pointer if used
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 10)]
+        public int[] unused;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudiobodyparts_t
+    {
+        public int sznameindex;
+        public int nummodels;
+        public int _base;
+        public int modelindex; // index into models array
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudiomodel_t
+    {
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 64)]
+        public char[] name;
+        
+        public int type;
+        
+        public float boundingradius;
+        
+        public int nummeshes;    
+        public int meshindex;
+
+        public int numvertices; // number of unique vertices/normals/texcoords
+        public int vertexindex; // vertex Vector
+        public int tangentsindex; // tangents Vector
+        
+        public int numattachments;
+        public int attachmentindex;
+        
+        public int numeyeballs;
+        public int eyeballindex;
+
+        public mstudio_modelvertexdata_t vertexdata;
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8)]
+        public int[] unused;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudiomesh_t
+    {
+        public int material;
+        public int modelindex;
+        
+        public int numvertices;
+        public int vertexoffset;
+        
+        public int numflexes;
+        public int flexoffset;
+        
+        public int materialtype;
+        public int materialparam;
+        
+        public int meshid;
+        
+        public Vector3 center;
+        public mstudio_meshvertexdata_t vertexdata;
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8)]
+        public int[] unused;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudio_modelvertexdata_t
+    {
+        public int vertexdata;
+        public int tangentdata;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudio_meshvertexdata_t
+    {
+        public int modelvertexdata;
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8)]
+        public int[] numlodvertices;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct vertexFileHeader_t
+    {
+        public int id; // MODEL_VERTEX_FILE_ID
+        public int version; // MODEL_VERTEX_FILE_VERSION
+
+        public int checksum; // same as studiohdr_t, ensures sync
+
+        public int numLODs; // num of valid lods
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8)]
+        public int[] numLODVertexes; // num verts for desired root lod
+
+        public int numFixups; // num of vertexFileFixup_t
+
+        public int fixupTableStart; // offset from base to fixup table
+        public int vertexDataStart; // offset from base to vertex block
+        public int tangentDataStart; // offset from base to tangent block
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct vertexFileFixup_t
+    {
+        public int lod; // used to skip culled root lod
+        public int sourceVertexID; // absolute index from start of vertex/tangent blocks
+        public int numVertexes;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudiovertex_t
+    {
+        public mstudioboneweight_t m_BoneWeights;
+        public Vector3 m_vecPosition;
+        public Vector3 m_vecNormal;
+        public Vector2 m_vecTexCoord;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct mstudioboneweight_t
+    {
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 3)]
+        public float[] weight;
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] bone; 
+
+        public byte numbones;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct FileHeader_t
+    {
+        public int version;
+
+        public int vertCacheSize;
+        public ushort maxBonesPerStrip;
+        public ushort maxBonesPerFace;
+        public int maxBonesPerVert;
+
+        public int checkSum;
+        
+        public int numLODs;
+
+        public int materialReplacementListOffset;
+        
+        public int numBodyParts;
+        public int bodyPartOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct BodyPartHeader_t
+    {
+        public int numModels;
+        public int modelOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct ModelHeader_t
+    {
+        public int numLODs;
+        public int lodOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct ModelLODHeader_t
+    {
+        public int numMeshes;
+        public int meshOffset;
+        public float switchPoint;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct MeshHeader_t
+    {
+        public int numStripGroups;
+        public int stripGroupHeaderOffset;
+        public byte flags;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct StripGroupHeader_t
+    {
+        public int numVerts;
+        public int vertOffset;
+        
+        public int numIndices;
+        public int indexOffset;
+        
+        public int numStrips;
+        public int stripOffset;
+        
+        public byte flags;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Vertex_t
+    {
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] boneWeightIndex;
+
+        public byte numBones;
+        
+        public ushort origMeshVertID;
+
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] boneID;
+    }
+}
---
+++ Core/MDL/StudioMdlLoader.cs
@@ -0,0 +1,269 @@
+﻿using UnityEngine;
+
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.IO;
+
+public class StudioMdlLoader : MdlSpecification
+{
+    private static CustomReader CRead;
+
+    private static studiohdr_t MDL_Header;
+    private static List<mstudiobodyparts_t> MDL_BodyParts = new List<mstudiobodyparts_t>();
+    private static List<mstudiomodel_t> MDL_Models = new List<mstudiomodel_t>();
+    private static List<mstudiomesh_t> MDL_Meshes = new List<mstudiomesh_t>();
+
+    private static List<string> MDL_TDirectories = new List<string>();
+    private static List<string> MDL_Textures = new List<string>();
+
+    private static List<Transform> MDL_Bones = new List<Transform>();
+
+    private static vertexFileHeader_t VVD_Header;
+    private static List<mstudiovertex_t> VVD_Vertexes = new List<mstudiovertex_t>();
+    private static List<vertexFileFixup_t> VVD_Fixups = new List<vertexFileFixup_t>();
+
+    private static FileHeader_t VTX_Header;
+    private static List<MeshHeader_t> VTX_Meshes = new List<MeshHeader_t>();
+
+    private static GameObject ModelObject;
+
+    private static void Clear()
+    {
+        MDL_BodyParts.Clear();
+        MDL_Models.Clear();
+        MDL_Meshes.Clear();
+
+        MDL_TDirectories.Clear();
+        MDL_Textures.Clear();
+
+        MDL_Bones.Clear();
+
+        VVD_Vertexes.Clear();
+        VVD_Fixups.Clear();
+
+        VTX_Meshes.Clear();
+        ModelObject = null;
+    }
+
+    public static Transform LoadMdl(string ModelName)
+    {
+        Clear();
+
+        string OpenPath = string.Concat(WorldController.GamePath, WorldController.ModName, ModelName);
+        ModelObject = new GameObject(ModelName);
+
+        if (!File.Exists(OpenPath + ".mdl"))
+            return ModelObject.transform;
+
+        // ----- BEGIN READ MDL, VVD, VTX ----- //
+
+        CRead = new CustomReader(new BinaryReader(File.OpenRead(OpenPath + ".mdl"))); 
+        MDL_Header = CRead.ReadType<studiohdr_t>(0);
+        ParseMdlFile();
+
+        CRead = new CustomReader(new BinaryReader(File.OpenRead(OpenPath + ".vvd"))); 
+        VVD_Header = CRead.ReadType<vertexFileHeader_t>(0);
+        ParseVvdFile();
+
+        CRead = new CustomReader(new BinaryReader(File.OpenRead(OpenPath + ".dx90.vtx"))); 
+        VTX_Header = CRead.ReadType<FileHeader_t>(0);
+        ParseVtxFile();
+
+        // ----- END READ MDL, VVD, VTX ----- //
+            
+        return ModelObject.transform;
+    }
+
+    private static void ParseMdlFile()
+    {
+        // ----- LOAD BODYPARTS ----- //
+
+        MDL_BodyParts.AddRange(CRead.ReadType<mstudiobodyparts_t>(MDL_Header.bodypart_offset, MDL_Header.bodypart_count));
+        int ModelArrayOffset = 0;
+
+        for (int i = 0; i < MDL_Header.bodypart_count; i++)
+        {
+            int ModelInputFilePosition = MDL_Header.bodypart_offset + (Marshal.SizeOf(typeof(mstudiobodyparts_t)) * i) + MDL_BodyParts[i].modelindex;
+            MDL_Models.AddRange(CRead.ReadType<mstudiomodel_t>(ModelInputFilePosition, MDL_BodyParts[i].nummodels));
+
+            for (int l = 0; l < MDL_BodyParts[i].nummodels; l++)
+            {
+                int MeshInputFilePosition = ModelInputFilePosition + (Marshal.SizeOf(typeof(mstudiomodel_t)) * l) + MDL_Models[ModelArrayOffset + l].meshindex;
+                MDL_Meshes.AddRange(CRead.ReadType<mstudiomesh_t>(MeshInputFilePosition, MDL_Models[l].nummeshes));
+            }
+
+            ModelArrayOffset += (MDL_BodyParts[i].nummodels - 1);
+        }
+
+        // ----- LOAD TEXTURES INFO ----- //
+
+        List<mstudiotexture_t> MDL_TexturesInfo = new List<mstudiotexture_t>();
+        MDL_TexturesInfo.AddRange(CRead.ReadType<mstudiotexture_t>(MDL_Header.texture_offset, MDL_Header.texture_count));
+
+        for (int i = 0; i < MDL_Header.texture_count; i++)
+        {
+            int StringInputFilePosition = MDL_Header.texture_offset + (Marshal.SizeOf(typeof(mstudiotexture_t)) * i) + MDL_TexturesInfo[i].sznameindex;
+            MDL_Textures.Add(CRead.ReadNullTerminatedString(StringInputFilePosition));
+        }
+
+        int[] TDirOffsets = CRead.ReadType<int>(MDL_Header.texturedir_offset, MDL_Header.texturedir_count);
+
+        for (int i = 0; i < MDL_Header.texturedir_count; i++)
+            MDL_TDirectories.Add(CRead.ReadNullTerminatedString(TDirOffsets[i]));
+
+        // ----- LOAD BONES ----- //
+
+        List<mstudiobone_t> MDL_BonesInfo = new List<mstudiobone_t>();
+        MDL_BonesInfo.AddRange(CRead.ReadType<mstudiobone_t>(MDL_Header.bone_offset, MDL_Header.bone_count));
+
+        for (int i = 0; i < MDL_Header.bone_count; i++)
+        {
+            int StringInputFilePosition = MDL_Header.bone_offset + (Marshal.SizeOf(typeof(mstudiobone_t)) * i) + MDL_BonesInfo[i].sznameindex;
+
+            GameObject BoneObject = new GameObject(CRead.ReadNullTerminatedString(StringInputFilePosition));
+            BoneObject.transform.parent = ModelObject.transform;
+
+            MDL_Bones.Add(BoneObject.transform);
+
+            if (MDL_BonesInfo[i].parent >= 0)
+                MDL_Bones[i].transform.parent = MDL_Bones[MDL_BonesInfo[i].parent].transform;
+        }
+    }
+
+    private static void ParseVtxFile()
+    {
+        List<BoneWeight> pBoneWeight = new List<BoneWeight>();
+        List<Vector3> pVertices = new List<Vector3>();
+        List<Vector3> pNormals = new List<Vector3>();
+        List<Vector2> pUvBuffer = new List<Vector2>();
+
+        // Load necessary information
+        mstudiomodel_t pModel = MDL_Models[0]; mstudiomesh_t pStudioMesh;
+        BodyPartHeader_t vBodypart = CRead.ReadType<BodyPartHeader_t>(VTX_Header.bodyPartOffset);
+
+        int ModelInputFilePosition = VTX_Header.bodyPartOffset + vBodypart.modelOffset;
+        ModelHeader_t vModel = CRead.ReadType<ModelHeader_t>(ModelInputFilePosition);
+
+        int ModelLODInputFilePosition = ModelInputFilePosition + vModel.lodOffset;
+        ModelLODHeader_t vLod = CRead.ReadType<ModelLODHeader_t>(ModelLODInputFilePosition);
+
+        int MeshInputFilePosition = ModelLODInputFilePosition + vLod.meshOffset;
+        VTX_Meshes.AddRange(CRead.ReadType<MeshHeader_t>(MeshInputFilePosition, vLod.numMeshes));
+
+        // Get bone weight's, vertices, normals, uv
+        for (int i = 0; i < pModel.numvertices; i++)
+        {
+            pBoneWeight.Add(GetBoneWeight(VVD_Vertexes[pModel.vertexindex + i].m_BoneWeights));
+
+            pVertices.Add(WorldController.SwapZY(VVD_Vertexes[pModel.vertexindex + i].m_vecPosition * WorldController.WorldScale));
+            pNormals.Add(WorldController.SwapZY(VVD_Vertexes[pModel.vertexindex + i].m_vecNormal));
+            pUvBuffer.Add(VVD_Vertexes[pModel.vertexindex + i].m_vecTexCoord);
+        }
+
+        GameObject meshObject = new GameObject(new string(pModel.name));
+        meshObject.transform.parent = ModelObject.transform;
+
+        SkinnedMeshRenderer smr = meshObject.AddComponent<SkinnedMeshRenderer>();
+        smr.materials = new Material[vLod.numMeshes];
+
+        // Calculate bindposes
+        Matrix4x4[] bindPoses = new Matrix4x4[MDL_Bones.Count];
+        for (int i = 0; i < bindPoses.Length; i++)
+        {
+            MDL_Bones[i].localPosition = Vector3.zero;
+            bindPoses[i] = MDL_Bones[i].worldToLocalMatrix * ModelObject.transform.localToWorldMatrix;
+        }
+
+        // Generate skin mesh
+        smr.sharedMesh = new Mesh();
+        smr.sharedMesh.name = new string(pModel.name);
+
+        smr.sharedMesh.subMeshCount = vLod.numMeshes;
+        smr.sharedMesh.vertices = pVertices.ToArray();
+        smr.sharedMesh.normals = pNormals.ToArray();
+        smr.sharedMesh.uv = pUvBuffer.ToArray();
+        
+        smr.sharedMesh.boneWeights = pBoneWeight.ToArray();
+        smr.sharedMesh.bindposes = bindPoses;
+
+        smr.sharedMesh.Optimize();
+
+        smr.bones = MDL_Bones.ToArray();
+        smr.updateWhenOffscreen = true;
+
+        for (int i = 0; i < vLod.numMeshes; i++)
+        {
+            List<int> pIndices = new List<int>();
+            
+            List<StripGroupHeader_t> StripGroups = new List<StripGroupHeader_t>();
+            int StripGroupFilePosition = MeshInputFilePosition + (Marshal.SizeOf(typeof(MeshHeader_t)) * i) + VTX_Meshes[i].stripGroupHeaderOffset;
+            StripGroups.AddRange(CRead.ReadType<StripGroupHeader_t>(StripGroupFilePosition, VTX_Meshes[i].numStripGroups)); pStudioMesh = MDL_Meshes[i];
+
+            // Get indices for model
+            for (int l = 0; l < VTX_Meshes[i].numStripGroups; l++)
+            {
+                List<Vertex_t> pVertexBuffer = new List<Vertex_t>();
+                pVertexBuffer.AddRange(CRead.ReadType<Vertex_t>(StripGroupFilePosition + (Marshal.SizeOf(typeof(StripGroupHeader_t)) * l) + StripGroups[l].vertOffset, StripGroups[l].numVerts));
+                
+                List<ushort> Indices = new List<ushort>();
+                Indices.AddRange(CRead.ReadType<ushort>(StripGroupFilePosition + (Marshal.SizeOf(typeof(StripGroupHeader_t)) * l) + StripGroups[l].indexOffset, StripGroups[l].numIndices));
+                
+                for (int n = 0; n < Indices.Count; n++)
+                    pIndices.Add(pVertexBuffer[Indices[n]].origMeshVertID + pStudioMesh.vertexoffset);
+            }
+            
+            smr.sharedMesh.SetTriangles(pIndices.ToArray(), i);
+            smr.materials[i].name = MDL_Textures[pStudioMesh.material];
+
+            string pathToTex = null; Material material = null;
+
+            // Find path to mesh material
+            for (int l = 0; l < MDL_TDirectories.Count; l++)
+                if (File.Exists(WorldController.DefaultTexPath + MDL_TDirectories[l] + MDL_Textures[pStudioMesh.material] + ".vmt"))
+                    pathToTex = WorldController.DefaultTexPath + MDL_TDirectories[l] + MDL_Textures[pStudioMesh.material] + ".vmt";
+
+            if (pathToTex != null)
+                material = ValveTextureLoader.LoadMaterial(pathToTex.Replace(WorldController.DefaultTexPath, ""));
+            
+            // Apply loaded material to mesh
+            if (material != null)
+            {
+                smr.materials[i].CopyPropertiesFromMaterial(material); 
+                smr.materials[i].shader = material.shader;
+            }
+        }
+    }
+
+    private static BoneWeight GetBoneWeight(mstudioboneweight_t mBoneWeight)
+    {
+        BoneWeight boneWeight = new BoneWeight();
+        
+        boneWeight.boneIndex0 = mBoneWeight.bone[0];
+        boneWeight.boneIndex1 = mBoneWeight.bone[1];
+        boneWeight.boneIndex2 = mBoneWeight.bone[2];
+        
+        boneWeight.weight0 = mBoneWeight.weight[0];
+        boneWeight.weight1 = mBoneWeight.weight[1];
+        boneWeight.weight2 = mBoneWeight.weight[2];
+        
+        return boneWeight;
+    }
+
+    private static void ParseVvdFile()
+    {
+        VVD_Fixups.AddRange(CRead.ReadType<vertexFileFixup_t>(VVD_Header.fixupTableStart, VVD_Header.numFixups));
+        VVD_Vertexes.AddRange(CRead.ReadType<mstudiovertex_t>(VVD_Header.vertexDataStart, VVD_Header.numLODVertexes[0]));
+
+        if (VVD_Header.numFixups > 0)
+            VVD_Vertexes.Clear();
+
+        // Apply fixup's for vertices
+        for (int i = 0; i < VVD_Header.numFixups; i++)
+        {
+            if (VVD_Fixups[i].lod >= 0)
+            {
+                VVD_Vertexes.AddRange(CRead.ReadType<mstudiovertex_t>(VVD_Header.vertexDataStart + (VVD_Fixups[i].sourceVertexID * 48), VVD_Fixups[i].numVertexes));
+            }
+        }
+    }
+}
